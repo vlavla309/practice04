8d14e2cdbbf4771571cc2763231c11fd0ba54f9f
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-22 17:27:53 +0900

    prob1 printer

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,6 +1,17 @@
 package prob01;
 
 public class Printer {
-	
+	public void println(int param) {
+		System.out.println(param);
+	}
+	public void println(boolean param) {
+		System.out.println(param);
+	}
+	public void println(double param) {
+		System.out.println(param);
+	}
+	public void println(String param) {
+		System.out.println(param);
+	}
 	
 }
